e2e3c7758e7c32a984db99bf6945350e99eababb
user-ПК\user <griphon56@gmail.com>
2017-04-29 23:41:05 +0300

    спрайт

---
+++ js/sprite.js
@@ -0,0 +1,10 @@
+var s_bird, s_bg, s_fg, s_pipeNorth, s_pipeSouth, s_text, s_score, s_splash, s_buttons, s_numberS, s_numberB;
+
+
+function Sprite(img, x, y, width, height) {
+    this.img = img;
+    this.x = x*2;
+    this.y = y*2;
+    this.width = width*2;
+    this.height = height*2;
+};
