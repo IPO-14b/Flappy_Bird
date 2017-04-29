076c1839e311ac73966ecb8a4dfd0d36856d70f4
user-ПК\user <griphon56@gmail.com>
2017-04-29 23:41:41 +0300

    Спрайт

--- js/sprite.js
+++ js/sprite.js
@@ -1,6 +1,14 @@
 var s_bird, s_bg, s_fg, s_pipeNorth, s_pipeSouth, s_text, s_score, s_splash, s_buttons, s_numberS, s_numberB;
 
-
+/**
+ *Спрайт класс
+ *
+ *@param {Image}  img    спрайт каринка
+ * @param {number} x      x-позиция в спрайте
+ * @param {number} y      y-позиция в спрайте
+ * @param {number} width  ширина спрайта 
+ * @param {number} height высота спрайта
+ */
 function Sprite(img, x, y, width, height) {
     this.img = img;
     this.x = x*2;
